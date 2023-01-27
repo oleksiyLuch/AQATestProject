72420d96a92862daf1bcd0fe6f723a0294260c04
Automation-Luch <oleksiyk@titanium-tech.net>
2023-01-27 09:39:19 +0200

    added makereport functionality with screenshot on fail

--- conftest.py
+++ conftest.py
@@ -1,7 +1,12 @@
+
+from pytest_html_reporter import attach
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
+
+
 
 @pytest.fixture()
 def driver():
@@ -11,8 +16,19 @@ def driver():
     options.add_argument('--disable-infobars')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
     options.add_argument('--remote-debugging-port=9222')
-    driver = webdriver.Chrome(options=options,service=driver_service)
+    options.add_argument(
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36")
+    driver = webdriver.Chrome(options=options, service=driver_service)
     driver.maximize_window()
     yield driver
     driver.quit()
+
+
+def pytest_runtest_makereport(item, call) -> None:
+    if call.when == "call":
+        if call.excinfo is not None and "driver" in item.funcargs:
+            page = item.funcargs["driver"]
+            attach(data=page.get_screenshot_as_png())
