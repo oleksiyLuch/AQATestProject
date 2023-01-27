ee94ef7d264356c8aba03016de4a72753fac5274
Automation-Luch <oleksiyk@titanium-tech.net>
2023-01-27 21:36:10 +0200

    ci/cd

--- conftest.py
+++ conftest.py
@@ -9,7 +9,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 def driver():
     driver_service = Service(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
-    options.add_argument('headless')
+    # options.add_argument('headless')
     options.add_argument('--disable-infobars')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
